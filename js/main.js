2ad78e645acdfea8d1245114a495b3e29c775497
Kurtko10 <kurtkoaf@gmail.com>
2024-01-18 17:11:49 +0100

    archivo JS, con funcciones que permite el encendido y apago, cambiando elementos y generando transicion en la pantalla. añadido de ñla carpeta sound

---
+++ js/main.js
@@ -0,0 +1,68 @@
+const App = {
+    init: function () {
+        App.animacion();
+    },
+
+    animacion: function () {
+        function whichTransitionEvent() {
+            let t;
+            const el = document.createElement('fake');
+            const transitions = {
+                'transition': 'transitionend',
+                'OTransition': 'oTransitionEnd',
+                'MozTransition': 'transitionend',
+                'WebkitTransition': 'webkitTransitionEnd'
+            };
+
+            for (t in transitions) {
+                if (el.style[t] !== undefined) {
+                    return transitions[t];
+                }
+            }
+        }
+
+        function playSound() {
+            console.log('Sonido Intro');
+            audio.currentTime = 0;
+            audio.play();
+        }
+
+        const audio = document.querySelector('audio');
+        const power = document.querySelector('.power');
+        const texto = document.querySelector('.animacionInicio');
+        const onOffBoton = document.querySelector('.on-off');
+
+        let isOn = false;
+
+        onOffBoton.onclick = function () {
+            if (isOn) {
+                App.btnOff();
+            } else {
+                App.btnOn();
+            }
+            isOn = !isOn;
+        };
+
+        App.btnOn = function () {
+            power.classList.add('power-on');
+
+            const transitionEvent = whichTransitionEvent();
+            texto.classList.add('end');
+            texto.addEventListener(transitionEvent, playSound);
+
+            const pantallaJuegoElement = document.querySelector('.gameboy .pantalla .pantallaJuego');
+            pantallaJuegoElement.style.background = '#ffd700';
+        };
+
+        App.btnOff = function () {
+            power.classList.remove('power-on');
+            texto.classList.remove('end');
+
+            const pantallaJuegoElement = document.querySelector('.gameboy .pantalla .pantallaJuego');
+            pantallaJuegoElement.style.background = '#bdae58';
+        };
+    }
+};
+
+App.init();
+
