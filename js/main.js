4366e25745e5d8142170129eee9da379151599fe
Kurtko10 <kurtkoaf@gmail.com>
2024-01-18 20:20:54 +0100

    añadido JS y css para el menu de juegos y sonido para el mismo

--- js/main.js
+++ js/main.js
@@ -23,11 +23,16 @@ const App = {
 
         function playSound() {
             console.log('Sonido Intro');
-            audio.currentTime = 0;
-            audio.play();
+            audioIntro.currentTime = 0;
+            audioIntro.play();
+        }
+        function playSound2(){
+            audioStart.currentTime = 0;
+            audioStart.play();
         }
 
-        const audio = document.querySelector('audio');
+        const audioIntro = document.getElementById('intro');
+        const audioStart = document.getElementById('start');
         const power = document.querySelector('.power');
         const texto = document.querySelector('.animacionInicio');
         const onOffBoton = document.querySelector('.on-off');
@@ -42,7 +47,53 @@ const App = {
             }
             isOn = !isOn;
         };
+//MOSTRAR SELECT DE JUEGOS
 
+        const btnControles = document.querySelector('.gameboy .btn-controles .btn-start-select');
+
+    btnControles.addEventListener('click', function (event) {
+    const clickedElement = event.target.id;
+
+    if (clickedElement === 'start') {
+        mostrarJuego();
+        
+        
+    }
+    });
+
+    function mostrarJuego() {
+        const juegoVisible = document.querySelector('.juego');
+        const juegos = document.querySelectorAll('.juego li');
+    
+        // Verifica si la propiedad display de .pulsaStart es 'inline-block'
+        if (document.querySelector('.pulsaStart').style.display === 'inline-block') {
+            playSound2();
+    
+            // Cambia el estilo del elemento para hacerlo visible
+            juegoVisible.style.display = 'block';
+    
+            // Añade la clase 'show' con retardo incremental
+            for (let i = 0; i < juegos.length; i++) {
+                setTimeout(function () {
+                    juegos[i].classList.add("show");
+                }, i * 500); // Ajusta este valor según sea necesario para el tiempo de retardo entre elementos
+    
+                // También puedes ajustar la lógica aquí para mostrar otros elementos o realizar acciones adicionales
+            }
+    
+            document.querySelector('.pulsaStart').style.display = 'none';
+            document.querySelector('.animacionInicio').style.display = 'none';
+            
+            texto.classList.remove('end');
+            // Puedes agregar estilos adicionales o cualquier otra configuración necesaria
+        }
+    }
+    
+
+
+       
+        
+        
         App.btnOn = function () {
             power.classList.add('power-on');
 
@@ -57,7 +108,7 @@ const App = {
 
                 setTimeout(function(){
                     document.querySelector('.pulsaStart').style.display = 'inline-block'; 
-                },2000);
+                },2500);
             });
 
             const pantallaJuegoElement = document.querySelector('.gameboy .pantalla .pantallaJuego');
@@ -71,6 +122,10 @@ const App = {
             const pantallaJuegoElement = document.querySelector('.gameboy .pantalla .pantallaJuego');
             pantallaJuegoElement.style.background = '#bdae58';
             document.querySelector('.pulsaStart').style.display = 'none';
+            document.querySelector('.juego').style.display = 'none';
+            document.querySelector('.animacionInicio').style.display = 'inline';
+            power.classList.remove('power-on');
+            texto.classList.remove('end');
         };
     }
 };
