99ec10189ce6f04742997c60b23514765d69712e
Kurtko10 <kurtkoaf@gmail.com>
2024-01-19 01:09:27 +0100

    añadido class'selector' en la seleccio de juegos

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 const App = {
     init: function () {
-        App.animacion();
+        App.viedoConsola();
     },
 
-    animacion: function () {
+    viedoConsola: function () {
         function whichTransitionEvent() {
             let t;
             const el = document.createElement('fake');
@@ -20,7 +20,9 @@ const App = {
                 }
             }
         }
-
+     
+        
+//Funciones para reproducir cada sound correspondiente
         function playSound() {
             console.log('Sonido Intro');
             audioIntro.currentTime = 0;
@@ -35,6 +37,8 @@ const App = {
             audioJuegosMusic.currentTime = 0;
             audioJuegosMusic.play();
         }
+
+//Variables para obtener elementos de DOM        
         let audioJuegosMusic = document.getElementById('juegosMusic');
         const audioIntro = document.getElementById('intro');
         const audioStart = document.getElementById('start');
@@ -42,8 +46,10 @@ const App = {
         const texto = document.querySelector('.animacionInicio');
         const onOffBoton = document.querySelector('.on-off');
 
+//Variable que para que el boton ON-OFF esté apagado        
         let isOn = false;
 
+//Función para el boton onOff        
         onOffBoton.onclick = function () {
             if (isOn) {
                 App.btnOff();
@@ -52,100 +58,111 @@ const App = {
             }
             isOn = !isOn;
         };
+
+
 //MOSTRAR SELECT DE JUEGOS
 
         const btnControles = document.querySelector('.gameboy .btn-controles .btn-start-select');
-
         btnControles.addEventListener('click', function (event) {
-        const clickedElement = event.target.id;
+            const clickedElement = event.target.id;
 
-    if (clickedElement === 'start') {
-        mostrarJuego();
-        
-        
-    }
-    });
+            if (clickedElement === 'start') {
+            mostrarJuego();
+            }
+        });
 
-    function mostrarJuego() {
-        const juegoVisible = document.querySelector('.juego');
-        const juegos = document.querySelectorAll('.juego li');
+        function mostrarJuego() {
+            const juegoVisible = document.querySelector('.juego');
+            const juegos = document.querySelectorAll('.juego li');
     
-        
-        if (document.querySelector('.pulsaStart').style.display === 'inline-block') {
-            playSound2();
-    
-            
-            juegoVisible.style.display = 'block';
+            if (document.querySelector('.pulsaStart').style.display === 'inline-block') {
+                playSound2();
+                juegoVisible.style.display = 'block';
             
             // Añade la clase 'show' con retardo incremental
-            for (let i = 0; i < juegos.length; i++) {
-                setTimeout(function () {
-                    juegos[i].classList.add("show");
-                }, i * 500); 
-            }
+                for (let i = 0; i < juegos.length; i++) {
+                    setTimeout(function () {
+                        juegos[i].classList.add("show");
+                    }, i * 500); 
+                }
             
-            document.querySelector('.pulsaStart').style.display = 'none';
-            document.querySelector('.animacionInicio').style.display = 'none';
-            texto.classList.remove('end');
-        }
-        if(document.querySelector('.juego').style.display === 'block' && audioJuegosMusic.paused){
-            playSound3();
-        }
+                document.querySelector('.pulsaStart').style.display = 'none';
+                document.querySelector('.animacionInicio').style.display = 'none';
+                texto.classList.remove('end');
+            }
+            if(document.querySelector('.juego').style.display === 'block' && audioJuegosMusic.paused){
+                playSound3();
+            }
         
-    }
+        }
+
+        document.addEventListener('DOMContentLoaded', function () {
+            const juegos = document.querySelectorAll('#juegos li');
     
-        App.btnOn = function () {
-            power.classList.add('power-on');
-            const transitionEvent = whichTransitionEvent();
-            texto.classList.add('end');
-            texto.addEventListener(transitionEvent, function(){
-                playSound();
+            juegos.forEach(function (juego) {
+                juego.addEventListener('click', function () {
+                ocultarTodasLasFlechas();
+    
+                const selector = juego.querySelector('.selector');
+                if (selector) {
+                    selector.style.display = 'inline-block';
+                }
+            });
+        });
+        
+        const juegoSeleccionado = document.querySelector('.seleccionado .selector');
+            if (juegoSeleccionado) {
+            juegoSeleccionado.style.display = 'inline-block';
+            }
+        });
+    
+        function ocultarTodasLasFlechas() {
+            document.querySelectorAll('.selector').forEach(function (selector) {
+            selector.style.display = 'none';
+            });
+        };
+
+    App.btnOn = function () {
+        power.classList.add('power-on');
+        const transitionEvent = whichTransitionEvent();
+        texto.classList.add('end');
+        texto.addEventListener(transitionEvent, function(){
+            playSound();
                 setTimeout(function(){
-                    document.querySelector('.pulsaStart').style.display = 'inline-block'; 
+                document.querySelector('.pulsaStart').style.display = 'inline-block'; 
                 },2500);
             });
 
-            const pantallaJuegoElement = document.querySelector('.gameboy .pantalla .pantallaJuego');
-            pantallaJuegoElement.style.background = '#ffd700';
-        };
-        document.addEventListener('DOMContentLoaded', function () {
-            const juegos = document.querySelectorAll('#juegos li');
-            
-            juegos.forEach(function (juego) {
-                juego.addEventListener('click', function () {
-                    // Oculta todas las flechas
-                    document.querySelectorAll('.selector').forEach(function (selector) {
-                        selector.style.display = 'none';
-                        
-                    });
+        const pantallaJuegoElement = document.querySelector('.gameboy .pantalla .pantallaJuego');
+        pantallaJuegoElement.style.background = '#ffd700';
+    };
+
         
-                    // Muestra la flecha solo en el elemento clicado
-                    const selector = juego.querySelector('.selector');
-                    if (selector) {
-                        selector.style.display = 'inline-block';
-                    }
-                });
-            });
-        });
+       
 
-        App.btnOff = function () {
-            power.classList.remove('power-on');
-            texto.classList.remove('end');
 
-            const pantallaJuegoElement = document.querySelector('.gameboy .pantalla .pantallaJuego');
-            pantallaJuegoElement.style.background = '#bdae58';
-            document.querySelector('.pulsaStart').style.display = 'none';
-            document.querySelector('.juego').style.display = 'none';
-            document.querySelector('.animacionInicio').style.display = 'inline';
-            power.classList.remove('power-on');
-            texto.classList.remove('end');
 
-            if (audioJuegosMusic && !audioJuegosMusic.paused) {
-                audioJuegosMusic.pause();
-                audioJuegosMusic.currentTime = 0; // Reinicia la posición de reproducción al principio del sonido
+
+    App.btnOff = function () {
+        power.classList.remove('power-on');
+        texto.classList.remove('end');
+
+        const pantallaJuegoElement = document.querySelector('.gameboy .pantalla .pantallaJuego');
+        pantallaJuegoElement.style.background = '#bdae58';
+        document.querySelector('.pulsaStart').style.display = 'none';
+        document.querySelector('.juego').style.display = 'none';
+        document.querySelector('.animacionInicio').style.display = 'inline';
+        power.classList.remove('power-on');
+        texto.classList.remove('end');
+
+        if (audioJuegosMusic && !audioJuegosMusic.paused) {
+            audioJuegosMusic.pause();
+            audioJuegosMusic.currentTime = 0; // Reinicia la posición de reproducción al principio del sonido
             }
-        };
-    }
+    };
+}
+
+
 };
 
 App.init();
